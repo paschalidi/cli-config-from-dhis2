d8cf358a3886bba79ff493b4c9b49cb6404137fe
paschalidi <paschalidi@outlook.com>
2020-04-16 19:25:42 +0200

    chore: adds husky

---
+++ .huskyrc.js
@@ -0,0 +1,12 @@
+const { config } = require('@dhis2/cli-style')
+const husky = require(config.husky)
+
+const tasks = arr => arr.join(' && ')
+
+module.exports = {
+    hooks: {
+        ...husky.hooks,
+        'pre-commit': tasks([
+        ]),
+    },
+}
