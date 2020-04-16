da30ba8f5880e9f1374d92d3a6f6e3d7709f9bc1
paschalidi <paschalidi@outlook.com>
2020-04-16 19:23:30 +0200

    chore: initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import SharingDialog from '@dhis2/d2-ui-sharing-dialog'
+
+function App() {
+    return (
+        <div className="App">
+            <SharingDialog />
+        </div>
+    )
+}
+
+export default App
+
+
